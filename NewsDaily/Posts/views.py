6b7500ef6b6b829624288a91f1eb1f68fb038c56
Alexandr Shevchenko <djpestro@gmail.com>
2024-11-17 10:50:59 +0600

    tmp

--- NewsDaily/Posts/views.py
+++ NewsDaily/Posts/views.py
@@ -1,3 +1,18 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+from Posts.models import Post
 
 # Create your views here.
+
+
+def index(request):
+    title = 'Главная страница'
+    posts = Post.objects.all().order_by('-public_date')
+    paginator = Paginator(posts, 4)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    
+    return render(request, 'index.html', {'page_obj': page_obj})
+
+
